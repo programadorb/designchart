12b2a7c5d5f062bf9735274ee69858ca85040605
Daniel molina <programanime@gmail.com>
2018-09-25 22:09:35 -0500

    evento onlistener removido

--- src/designchart.clases.js
+++ src/designchart.clases.js
@@ -3,22 +3,21 @@ function BarChart(id, title, labels, datasets){
         labels: labels,
         datasets: datasets
     };
-    window.addEventListener("load", function(){
-        var ctx = document.getElementById(id).getContext('2d');
-        window.myBar = new Chart(ctx, {
-            type: 'bar',
-            data: barChartData,
-            options: {
-                responsive: true,
-                legend: {
-                    position: 'top',
-                },
-                title: {
-                    display: true,
-                    text: title
-                }
+
+    var ctx = document.getElementById(id).getContext('2d');
+    window.myBar = new Chart(ctx, {
+        type: 'bar',
+        data: barChartData,
+        options: {
+            responsive: true,
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: title
             }
-        });
+        }
     });
 }
 
@@ -35,82 +34,76 @@ function PieChart(id, title, labels, datasets){
         }
     };
 
-    window.addEventListener("load", function(){
-        var ctx = document.getElementById(id).getContext('2d');
-	    new Chart(ctx, config);
-    });    
+    var ctx = document.getElementById(id).getContext('2d');
+    new Chart(ctx, config);
 }
 
 function ProcessChart(id, code){
-    window.addEventListener("load", function(){
-        var chart = flowchart.parse(code);
-        chart.drawSVG(id, {
-            'line-width': 3,
-            'maxWidth': 3,
-            'line-length': 50,
-            'text-margin': 10,
-            'font-size': 14,
-            'font': 'normal',
-            'font-family': 'Helvetica',
-            'font-weight': 'normal',
-            'font-color': 'black',
-            'line-color': 'black',
-            'element-color': 'black',
-            'fill': 'white',
-            'yes-text': 'yes',
-            'no-text': 'no',
-            'arrow-end': 'block',
-            'scale': 1,
-            'symbols': {
-            'start': {'font-color': 'red','element-color': 'green','fill': 'yellow'},
-            'end':{'class': 'end-element'}
-            },
-            'flowstate' : {
-            'past' : { 'fill' : '#CCCCCC', 'font-size' : 12},
-            'current' : {'fill' : 'yellow', 'font-color' : 'red', 'font-weight' : 'bold'},
-            'future' : { 'fill' : '#FFFF99'},
-            'request' : { 'fill' : 'blue'},
-            'invalid': {'fill' : '#444444'},
-            'approved' : { 'fill' : '#58C4A3', 'font-size' : 12, 'yes-text' : 'APPROVED', 'no-text' : 'n/a' },
-            'rejected' : { 'fill' : '#C45879', 'font-size' : 12, 'yes-text' : 'n/a', 'no-text' : 'REJECTED' }
-            }
-        });
-    });       
+    var chart = flowchart.parse(code);
+    chart.drawSVG(id, {
+        'line-width': 3,
+        'maxWidth': 3,
+        'line-length': 50,
+        'text-margin': 10,
+        'font-size': 14,
+        'font': 'normal',
+        'font-family': 'Helvetica',
+        'font-weight': 'normal',
+        'font-color': 'black',
+        'line-color': 'black',
+        'element-color': 'black',
+        'fill': 'white',
+        'yes-text': 'yes',
+        'no-text': 'no',
+        'arrow-end': 'block',
+        'scale': 1,
+        'symbols': {
+        'start': {'font-color': 'red','element-color': 'green','fill': 'yellow'},
+        'end':{'class': 'end-element'}
+        },
+        'flowstate' : {
+        'past' : { 'fill' : '#CCCCCC', 'font-size' : 12},
+        'current' : {'fill' : 'yellow', 'font-color' : 'red', 'font-weight' : 'bold'},
+        'future' : { 'fill' : '#FFFF99'},
+        'request' : { 'fill' : 'blue'},
+        'invalid': {'fill' : '#444444'},
+        'approved' : { 'fill' : '#58C4A3', 'font-size' : 12, 'yes-text' : 'APPROVED', 'no-text' : 'n/a' },
+        'rejected' : { 'fill' : '#C45879', 'font-size' : 12, 'yes-text' : 'n/a', 'no-text' : 'REJECTED' }
+        }
+    });
 }
 
 function FlowChart(id, code){
-    window.addEventListener("load", function(){
-        var chart = flowchart.parse(code);
-        chart.drawSVG(id, {
-            'line-width': 3,
-            'maxWidth': 3,
-            'line-length': 50,
-            'text-margin': 10,
-            'font-size': 14,
-            'font': 'normal',
-            'font-family': 'Helvetica',
-            'font-weight': 'normal',
-            'font-color': 'black',
-            'line-color': 'black',
-            'element-color': 'black',
-            'fill': 'white',
-            'yes-text': 'yes',
-            'no-text': 'no',
-            'arrow-end': 'block',
-            'scale': 1,
-            'symbols': {
-            'start': {'font-color': 'red','element-color': 'green','fill': 'yellow'},
-            'end':{'class': 'end-element'}
-            },
-            'flowstate' : {
-            'past' : { 'fill' : '#CCCCCC', 'font-size' : 12},
-            'current' : {'fill' : 'yellow', 'font-color' : 'red', 'font-weight' : 'bold'},
-            'future' : { 'fill' : '#FFFF99'},
-            'request' : { 'fill' : 'blue'},
-            'invalid': {'fill' : '#444444'},
-            'approved' : { 'fill' : '#58C4A3', 'font-size' : 12, 'yes-text' : 'APPROVED', 'no-text' : 'n/a' },
-            'rejected' : { 'fill' : '#C45879', 'font-size' : 12, 'yes-text' : 'n/a', 'no-text' : 'REJECTED' }
-            }
-        });
-    });       
+    var chart = flowchart.parse(code);
+    chart.drawSVG(id, {
+        'line-width': 3,
+        'maxWidth': 3,
+        'line-length': 50,
+        'text-margin': 10,
+        'font-size': 14,
+        'font': 'normal',
+        'font-family': 'Helvetica',
+        'font-weight': 'normal',
+        'font-color': 'black',
+        'line-color': 'black',
+        'element-color': 'black',
+        'fill': 'white',
+        'yes-text': 'yes',
+        'no-text': 'no',
+        'arrow-end': 'block',
+        'scale': 1,
+        'symbols': {
+        'start': {'font-color': 'red','element-color': 'green','fill': 'yellow'},
+        'end':{'class': 'end-element'}
+        },
+        'flowstate' : {
+        'past' : { 'fill' : '#CCCCCC', 'font-size' : 12},
+        'current' : {'fill' : 'yellow', 'font-color' : 'red', 'font-weight' : 'bold'},
+        'future' : { 'fill' : '#FFFF99'},
+        'request' : { 'fill' : 'blue'},
+        'invalid': {'fill' : '#444444'},
+        'approved' : { 'fill' : '#58C4A3', 'font-size' : 12, 'yes-text' : 'APPROVED', 'no-text' : 'n/a' },
+        'rejected' : { 'fill' : '#C45879', 'font-size' : 12, 'yes-text' : 'n/a', 'no-text' : 'REJECTED' }
+        }
+    });
 }
